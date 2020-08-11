dbbf3d6ed6f719e67ce36b50566b8949b97d1b30
robcolyn <69480206+robcolyn@users.noreply.github.com>
2020-08-11 17:30:13 -0600

    Update 3_Model assessment and threshold selection.R

--- 3_Model assessment and threshold selection.R
+++ 3_Model assessment and threshold selection.R
@@ -1,16 +1,16 @@
 #Assessing thresholds to convert models from continous to binary ouputs
-#Additionally allows for asessment of model fit - AUc, ROC plots, etc. 
+#Additionally allows for asessment of model fit - AUC, ROC plots, etc. 
 
 library(PresenceAbsence)
 
 #Extract predicted suitability values from SDM for both presence and absence points
 #A better approach here is to limit backgorund points to known areas of absence. Using
-#the circular window method (see SDm script) is a good method of generating background 
-#points in the absence of true absence data.
+#the circular window method (see SDM script) is a good method of generating constrained 
+#background points if no absence data is available.
 evaldata <- raster::extract(BiomodEM, sdmdata) 
 
 #Create histogram of predicted suitability to that of presence and absence localities
-par(mfrow=c(1,1))
+par(mfrow=c(1,1)) #amend as required
 for(i in 1:3)
 {presence.absence.hist(DATA = evaldata, na.rm = TRUE, which.model = 1, model.names=c("Southern Bald Ibis"), 
                        N.bars=10, truncate.tallest=TRUE, opt.thresholds=TRUE, xlab = "Predicted probability",
@@ -25,8 +25,9 @@ error.threshold.plot(DATA = evaldata, na.rm = TRUE, which.model = 1, model.names
 auc.roc.plot(DATA = evaldata, color=TRUE, main = "Southern Bald Ibis")
 
 #Replicate the above graphs using the dataset withheld for model
-#validation. Subistute presence points to only include validation
-#data and assess AUC-ROC plots and histograms of predicted suitability. 
+#validation. Substitute presence points to only include validation
+#data. Assess ommision rates, AUC-ROC plots and histograms of predicted 
+#suitability. 
 
 #The threshold selected should ideally limit ommision rates of presence 
 #data, produce lower error rates, and higher AUC values. 
